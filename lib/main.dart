57f345a8a5ae23f356f34787f150095014965c3a
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-02-26 13:51:54 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,30 +3,75 @@ import 'package:provider/provider.dart';
 
 import 'services/cart_service.dart';
 import 'screens/home_screen.dart';
+import 'screens/cart_screen.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => CartService(),
-      child: const MyApp(),
-    ),
-  );
+  runApp(const CrudoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CrudoApp extends StatelessWidget {
+  const CrudoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CRUDO',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        scaffoldBackgroundColor: const Color(0xFFF5F5F5),
-        fontFamily: 'Roboto',
+    return ChangeNotifierProvider(
+      create: (_) => CartService(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'CRUDO',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          scaffoldBackgroundColor: const Color(0xFFF5F5F5),
+        ),
+        home: const MainNavigation(),
       ),
-      home: const HomeScreen(),
+    );
+  }
+}
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int currentIndex = 0;
+
+  final List<Widget> screens = const [
+    HomeScreen(),
+    CartScreen(),
+    Center(child: Text("Profile coming soon")),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.green,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: "Cart",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+      ),
     );
   }
 }
