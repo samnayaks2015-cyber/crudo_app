0f867bad346db51c744cf1b22227507f51ed66aa
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-02-28 03:00:37 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'services/cart_service.dart';
+import 'screens/home_screen.dart';
+import 'screens/cart_screen.dart';
 
 void main() {
-  runApp(const CrudoApp());
+  runApp(const MyApp());
 }
 
-class CrudoApp extends StatelessWidget {
-  const CrudoApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,65 +18,11 @@ class CrudoApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Crudo Store',
-        theme: ThemeData(primarySwatch: Colors.green),
-        home: const HomeScreen(),
+        routes: {
+          '/': (context) => const HomeScreen(),
+          CartScreen.routeName: (context) => const CartScreen(),
+        },
       ),
     );
   }
 }
-
-//////////////////////
-// MODEL
-//////////////////////
-
-class CartItem {
-  final String name;
-  final double price;
-
-  CartItem({required this.name, required this.price});
-}
-
-//////////////////////
-// SERVICE
-//////////////////////
-
-class CartService extends ChangeNotifier {
-  final List<CartItem> _items = [];
-
-  List<CartItem> get items => _items;
-
-  int get count => _items.length;
-
-  double get totalPrice =>
-      _items.fold(0.0, (sum, item) => sum + item.price);
-
-  void addItem(String name, double price) {
-    _items.add(CartItem(name: name, price: price));
-    notifyListeners();
-  }
-
-  void removeItem(int index) {
-    _items.removeAt(index);
-    notifyListeners();
-  }
-}
-
-//////////////////////
-// HOME SCREEN
-//////////////////////
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final cart = Provider.of<CartService>(context);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Crudo Store'),
-        actions: [
-          Stack(
-            children: [
-              IconButton(
-                icon
