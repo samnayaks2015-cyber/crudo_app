1a689bc44990321b505a36a2ce6fc6d18ecd40ae
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-02-28 12:57:00 -0500

    Create main_shell.dart

---
+++ lib/screens/main_shell.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'cart_screen.dart';
+
+class MainShell extends StatefulWidget {
+  const MainShell({super.key});
+
+  @override
+  State<MainShell> createState() => _MainShellState();
+}
+
+class _MainShellState extends State<MainShell> {
+  int index = 0;
+
+  final pages = const [
+    HomeScreen(),
+    CartScreen(),
+    Center(child: Text("Profile Coming Soon")),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (i) => setState(() => index = i),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: "Cart"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
