564479e54a3b4a555f41ece99f19dbb176f35da8
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-02-28 12:51:33 -0500

    Create product.dart

---
+++ lib/lib/models/product.dart
@@ -0,0 +1,11 @@
+class Product {
+  final String name;
+  final double price;
+  final String category;
+
+  Product({
+    required this.name,
+    required this.price,
+    required this.category,
+  });
+}
