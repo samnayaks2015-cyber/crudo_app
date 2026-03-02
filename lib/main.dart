983815aaa65943d7a8ba54ac874e4ef3ce485feb
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-02 17:11:55 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
-import 'screens/cart_screen.dart';
-import 'services/cart_service.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,59 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainNavigation(),
-    );
-  }
-}
-
-class MainNavigation extends StatefulWidget {
-  const MainNavigation({super.key});
-
-  @override
-  State<MainNavigation> createState() => _MainNavigationState();
-}
-
-class _MainNavigationState extends State<MainNavigation> {
-  int _selectedIndex = 0;
-  final CartService cart = CartService();
-
-  void _openCart() {
-    setState(() {
-      _selectedIndex = 1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final List<Widget> pages = [
-      HomeScreen(
-        cart: cart,
-        onCartPressed: _openCart,
-      ),
-      CartScreen(cart: cart),
-    ];
-
-    return Scaffold(
-      body: pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.green,
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: 'Cart',
-          ),
-        ],
-      ),
+      home: HomeScreen(),
     );
   }
 }
