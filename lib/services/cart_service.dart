41232c7f672335552ca7bf31c5507b511f5287ad
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-01 23:17:27 -0500

    Update cart_service.dart

--- lib/services/cart_service.dart
+++ lib/services/cart_service.dart
@@ -1,16 +1,25 @@
-class CartService {
-  final List<Map<String, dynamic>> _items = [];
+import '../models/cart_item.dart';
 
-  List<Map<String, dynamic>> get items => _items;
+class CartService {
+  static final CartService instance = CartService._internal();
+
+  factory CartService() => instance;
+
+  CartService._internal();
+
+  final List<CartItem> _items = [];
+
+  List<CartItem> get items => _items;
 
   void addItem({required String name, required double price}) {
-    _items.add({
-      'name': name,
-      'price': price,
-    });
+    try {
+      final existing = _items.firstWhere((e) => e.name == name);
+      existing.quantity++;
+    } catch (e) {
+      _items.add(CartItem(name: name, price: price));
+    }
   }
 
-  void clearCart() {
-    _items.clear();
-  }
+  double get total =>
+      _items.fold(0, (sum, item) => sum + (item.price * item.quantity));
 }
