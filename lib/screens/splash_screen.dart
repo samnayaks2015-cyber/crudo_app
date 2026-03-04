50bc0b2ae6a13a5a0685bf7608d8cc9eddd191e7
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-05 02:25:47 +0530

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../main.dart';
+import 'home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,28 +9,31 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
   @override
   void initState() {
+
     super.initState();
 
-    Future.delayed(const Duration(seconds: 2), () {
+    Future.delayed(const Duration(seconds:2),(){
+
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (_) => const MainNavigation()),
+        MaterialPageRoute(builder: (_) => const HomeScreen())
       );
+
     });
+
   }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      backgroundColor: Colors.white,
       body: Center(
-        child: Image.asset(
-          'assets/images/logo.png',
-          height: 120,
-        ),
+        child: Image.asset("assets/images/logo.png",height:120),
       ),
     );
+
   }
 }
