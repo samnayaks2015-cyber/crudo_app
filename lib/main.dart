1a03acda8d2ea52ec50bec185390f543d4c83bb2
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-05 08:55:27 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-
-import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/cart_screen.dart';
 import 'screens/profile_screen.dart';
 import 'utils/theme.dart';
 
-void main() async {
-
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
+void main() {
   runApp(const CrudoApp());
 }
 
@@ -19,27 +14,17 @@ class CrudoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
-
       title: "CRUDO",
-
       theme: AppTheme.lightTheme,
-
       initialRoute: "/login",
-
       routes: {
-
         "/login": (context) => const LoginScreen(),
-
         "/home": (context) => const HomeScreen(),
-
+        "/cart": (context) => const CartScreen(),
         "/profile": (context) => const ProfileScreen(),
-
       },
-
     );
   }
 }
