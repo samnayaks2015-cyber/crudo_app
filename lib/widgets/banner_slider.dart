cf41c4d617c2dbba80c77451fdf3ed753491000f
samnayaks2015-cyber <samnayaks2015@gmail.com>
2026-03-05 09:53:45 +0530

    Update banner_slider.dart

--- lib/widgets/banner_slider.dart
+++ lib/widgets/banner_slider.dart
@@ -9,14 +9,15 @@ class BannerSlider extends StatefulWidget {
 
 class _BannerSliderState extends State<BannerSlider> {
 
+  final PageController controller = PageController();
+
+  int currentIndex = 0;
+
   final List<String> banners = [
     "assets/images/banner1.png",
     "assets/images/banner2.png",
     "assets/images/banner3.png",
   ];
-
-  int currentIndex = 0;
-  final PageController controller = PageController();
 
   @override
   Widget build(BuildContext context) {
@@ -29,11 +30,53 @@ class _BannerSliderState extends State<BannerSlider> {
           child: PageView.builder(
             controller: controller,
             itemCount: banners.length,
-            onPageChanged: (index){
+            onPageChanged: (index) {
               setState(() {
                 currentIndex = index;
               });
             },
-            itemBuilder: (context,index){
+            itemBuilder: (context, index) {
+
               return Container(
-                margin: const
+                margin: const EdgeInsets.symmetric(horizontal: 12),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                    image: AssetImage(banners[index]),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+
+            },
+          ),
+        ),
+
+        const SizedBox(height: 10),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+            banners.length,
+            (index) {
+
+              return Container(
+                margin: const EdgeInsets.symmetric(horizontal: 4),
+                width: currentIndex == index ? 10 : 6,
+                height: currentIndex == index ? 10 : 6,
+                decoration: BoxDecoration(
+                  color: currentIndex == index
+                      ? Colors.green
+                      : Colors.grey,
+                  shape: BoxShape.circle,
+                ),
+              );
+
+            },
+          ),
+        ),
+
+      ],
+    );
+  }
+}
